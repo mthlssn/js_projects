ec0d3416c6486cdb7a9d697b4d6a98ff028f45f2
mthlssn <mthlssn@gmail.com>
2023-12-20 12:48:23 -0300

    update calculadora

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,52 +1,108 @@
-
 var operacao = ''
 
-var num1 = 0
-var num2 = 0
+var num1 = ''
+var num2 = ''
 var res = 0
+
+var float = false
 
 var vez_num = 1
 
-function tecla_precionada(tecla) {
+function tecla_precionada(tecla) 
+{
+    if (tecla == 'c')
+    {
+        num1 = ''
+        num2 = ''
+        res = 0
 
-    if ( typeof tecla == "number" ){
-        switch (vez_num){
+        float = false
+
+        vez_num = 1
+
+        mostrar('')
+    }
+    else if ( typeof tecla == "number" || tecla == '.')
+    {
+        var temp
+
+        switch (vez_num)
+        {
             case 1:
-                var temp = (num1 * 10) + tecla
-
-                if ( 999999999 >= temp)
-                    num1 = temp
-
-                mostrar(num1)
+                temp = num1
 
                 break;
 
             case 2:
-                var temp = (num2 * 10) + tecla
-
-                if ( 999999999 >= temp)
-                    num2 = temp
-
-                mostrar(num2)
+                if (operacao == '')
+                {
+                    vez_num = 1
+                    temp = ''
+                } else
+                    temp = num2
 
                 break;
         }
+        
+        if (tecla == '.') 
+        {
+            if (temp.length == 0)
+            {
+                temp = '0.'
+                float = true
+                
+            } else if (temp.length <= 8 && float == false)
+            {
+                temp = temp + tecla
+                float = true
+            }
+            } else 
+            {
+                if (float && temp.length <= 9) 
+                    temp = temp + tecla
+                else if ( !float && temp.length <= 8)
+                    temp = temp + tecla
+            }
+        
+        switch (vez_num)
+        {
+            case 1:
+                num1 = temp
+                break;
+    
+            case 2:
+                num2 = temp
+                break;
+        }
 
-    } else {
+        mostrar(temp)
+
+    } else 
+    {
         vez_num = 2
+        float = false
 
-        if (tecla == '=') 
-            realizar_conta()
+        if (tecla == '=')
+        {
+            realizar_conta(operacao)
+            operacao = ''
+        }
+        else if (operacao != '')
+        {
+            realizar_conta(operacao)
+            operacao = tecla
+        }
         else
             operacao = tecla
-
     }
 }
 
-function realizar_conta() {
-    console.log("NUM 01:" + num1)
-    console.log("NUM 02:" + num2)
-    switch (operacao){
+function realizar_conta(ope) 
+{
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+
+    switch (ope){
         case '+':
             res = num1 + num2;
             break;
@@ -64,12 +120,16 @@ function realizar_conta() {
             break;
     }
 
-    console.log("RES: " + res);
+    if ( res > 999999999.0 )
+        res = 'error'
+    else
+        res = res.toFixed(2) 
+
     mostrar(res)
 
-    operacao = ''
-
-    vez_num = 1
+    num1 = '' + res
+    num2 = ''
+    
 
 }
 
