4a79305b37a7f35223b333f7637d37653e12feaa
mthlssn <mthlssn@gmail.com>
2024-01-23 06:30:32 -0300

    getbyid, update and delete

--- task_list/routes/routes.js
+++ task_list/routes/routes.js
@@ -3,5 +3,9 @@ const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTasks);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask)
+
 
 module.exports = routes;
