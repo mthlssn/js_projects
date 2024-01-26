f19ebbc11092fcf503878304e3c33807246f4fbb
mthlssn <mthlssn@gmail.com>
2024-01-26 19:30:51 -0300

    deploy configs

--- task_list/database/db.js
+++ task_list/database/db.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const connectToDb = () => {
     mongoose
         .connect(
-            "mongodb+srv://root:admin@teste.vyipdud.mongodb.net/?retryWrites=true&w=majority",
-            // {
-            //     useNewUrlParser: true,
-            //     useUnifiedTopology: true,
-            // }
+            process.env.DB_URI,
         )
         .then(() => console.log("MongoDB Atlas CONECTADO!"))
         .catch((err) => console.log(err));
