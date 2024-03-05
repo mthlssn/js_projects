d3381ea81db7f51f3445fd4544f300adbf4b2a63
mthlssn <mthlssn@gmail.com>
2024-03-04 23:39:12 -0300

    updade gerar() - map generator

--- map_generator/js/script.js
+++ map_generator/js/script.js
@@ -1,54 +1,79 @@
+const screen = document.getElementById('screen');
+const context = screen.getContext('2d');
 
+const tamanhox = 1920;
+
+const tamanhoy = 1080;
+
+const width = 1;
+const height = 1;
+
+var a = 0;
 
 function gerar(){
+  
+  var tamanho_quadrado = 100;
 
-    const screen = document.getElementById('screen')
-    const context = screen.getContext('2d')
+  // context.fillStyle = "#000"
 
-    const tamanhox = 1920
-    const taRanX = 640
+  context.clearRect(0 , 0, tamanhox, tamanhoy);
 
-    const tamanhoy = 1080
-    const taRanY = 360
+  context.fillStyle = "#000";
+  context.fillRect( 0, 0, tamanhox, tamanhoy);
 
-    const width = 1
-    const height = 1
+  for (let i = 0; i < 10; i++) {
 
-    var tamanho_quadrado = 100
+    // tamanho aleatorio dos continentes
+    let ranC_width = Math.floor(Math.random() * ((tamanhox / 3) * 2.5 - (tamanhox / 6)) + tamanhox / 6);
+    let ranC_height = Math.floor(Math.random() * ((tamanhoy / 3) * 2.5 - (tamanhox / 6)) + tamanhox / 6);
 
-    var color = ''
-    var positionX = 0
-    var positionY = 0
+    // posição aleatória dos continentes
+    let ranC_posX = somaRan(20, tamanhox - ranC_width, "separar");
+    let ranC_posY = somaRan(20, tamanhoy - ranC_height, "separar");
 
-    var array = []
-    var array2 = []
+    for (let i = 0; i < 250; i++) {
 
-    // context.fillStyle = "#000"
+      // tamanho aleatorio das partes do continente
+      let ran_tamanho = somaRan(2, tamanho_quadrado, "juntar");
 
-    context.clearRect(0,0,tamanhox,tamanhoy);
+      // posição aleatória das partes do continente
+      let positionX = somaRan(6, ranC_width - ran_tamanho, "juntar") + ranC_posX;  
+      let positionY = somaRan(6, ranC_height - ran_tamanho, "juntar") + ranC_posY; 
 
-    context.fillStyle = "rgba(0, 0, 0, 0)"
-    context.fillRect( 0, 0, tamanhox, tamanhoy)
-
-    for (let i = 0; i < 100; i++) {
-        let ran_tamanho = Math.floor(Math.random() * tamanho_quadrado) + Math.floor(Math.random() * tamanho_quadrado);3
-
-        positionX =  Math.floor(Math.random() * taRanX) + Math.floor(Math.random() * taRanX) + Math.floor(Math.random() * taRanX);
-        positionY =  Math.floor(Math.random() * taRanY) + Math.floor(Math.random() * taRanY) + Math.floor(Math.random() * taRanY);
-
-        positionX = positionX - Math.floor(ran_tamanho/2)
-        positionY = positionY - Math.floor(ran_tamanho/2)
-
-        context.fillStyle = "#fff"
-        context.fillRect( positionX, positionY, ran_tamanho, ran_tamanho)
-
-        console.log(positionX, positionY, ran_tamanho);
+      // parte do continente
+      context.fillStyle = "#fff";
+      context.fillRect( positionX, positionY, ran_tamanho, ran_tamanho);
     }
+  }
 }
 
+// soma número aleatório para juntar ou separar
+function somaRan (vezes, num, tipo) {
+  let temp = 0;
+  for (let i = 0; i < vezes; i++) {
+    switch (tipo){
+      case "juntar":
+        temp = Math.floor(Math.random() * num/vezes) + temp; 
+        break;
+      case "separar":
+        temp = Math.floor(Math.random() * num/vezes) + temp;
+
+        if (i+1 == vezes) {
+          temp = num/2 - temp;
+          if (temp <= 0) {
+            temp = num - Math.abs(temp);
+          }
+        }
+        break;
+    }
+  }
+  return temp;
+}
+
+// download do canvas em png
 function download() {
 
-  const canvas = document.getElementById('screen')
+  const canvas = document.getElementById('screen');
 
   const imgData = canvas.toDataURL("img");
 
